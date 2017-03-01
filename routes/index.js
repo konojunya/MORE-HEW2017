a3780f0d938e94a1d23b217cd4f168cac375ad63
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2017-03-01 17:04:36 +0900

    :+1: いい感じにした

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.get('/mypage', function(req, res, next) {
+  res.render('mypage');
+});
+router.get('/spot', function(req, res, next) {
+  res.render('spot-list');
+});
+router.get('/spot/detail', function(req, res, next) {
+  res.render('spot-detail');
 });
 
 module.exports = router;
